e31099dca34806ad122642daad0501cb0703bd41
Tang Xing <54627493+a634086776@users.noreply.github.com>
2023-12-07 23:46:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '7ce125e5-570a-4ee3-ac7f-2cce30cd9ebd';
+let userID = '31b519de-00ac-4516-bf67-52a9e3a0e43c';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
